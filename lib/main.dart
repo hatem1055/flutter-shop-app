a1a485e29aac32cf4f2bb1328a5cabc703b37720
hatem1055 <hatemmostafa31@gmail.com>
2020-11-30 22:40:54 +0200

    using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:shoppy/screens/product_details/product_details_screen.dart';
+import 'package:provider/provider.dart';
+import 'screens/product_details/product_details_screen.dart';
 import 'screens/products_overview/products_overview_screen.dart';
+import 'providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shoppy',
-      initialRoute: '/',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor:Colors.deepOrange,
-        fontFamily: 'Lato'
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Shoppy',
+        initialRoute: '/',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        routes: {
+          '/': (ctx) => ProductOverviewScreen(),
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()
+        },
       ),
-      routes: {
-        '/': (ctx) => ProductOverviewScreen(),
-        ProductDetailsScreen.routeName:(ctx) => ProductDetailsScreen()
-      },
     );
   }
 }
-
-
---
+++ lib/screens/products_overview/products_grid.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/products_provider.dart';
+import 'product_widget.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: productsData.products.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (_, index) {
+          return ProductWidget(productsData.products[index]);
+        });
+  }
+}
