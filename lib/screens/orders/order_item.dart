3fc62b9f5d97618ee990b486f3acb23a46031e8a
hatem1055 <hatemmostafa31@gmail.com>
2020-12-25 21:42:52 +0200

    order item

--- lib/screens/orders/order_item.dart
+++ lib/screens/orders/order_item.dart
@@ -1,26 +1,61 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../providers/orders_provider.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
   OrderItemWidget(this.order);
+
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var isExpaneded = false;
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: Column(
-        children:[
-          ListTile(
-            title:Text('\$${order.amount}'),
-            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(order.dateTime)),
-            trailing: IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.expand_more),
+      child: Column(children: [
+        ListTile(
+          title: Text('\$${widget.order.amount}'),
+          subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime)),
+          trailing: IconButton(
+            onPressed: () {
+              setState(() {
+                isExpaneded = !isExpaneded;
+              });
+            },
+            icon: Icon(isExpaneded ? Icons.expand_less : Icons.expand_more),
+          ),
+        ),
+        if (isExpaneded)
+          Container(
+            padding: EdgeInsets.symmetric(horizontal:15,vertical:4),
+            height: min(widget.order.products.length * 20.0 + 30, 180),
+            child: ListView(
+              children: widget.order.products
+                  .map((product) => Container(
+                    margin: EdgeInsets.only(top:10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                          Text(product.title,
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                          Text(
+                            '${product.quantity}x \$${product.price}',
+                            style: TextStyle(fontSize: 18, color: Colors.grey),
+                          )
+                        ]),
+                  ))
+                  .toList(),
             ),
           )
-        ]
-      ),
+      ]),
     );
   }
 }
